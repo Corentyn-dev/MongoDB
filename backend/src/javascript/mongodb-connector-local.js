a9b14246951763f0fe94798f0dc2e301a29fe599
CocoCoding <115353585+Corentyn-dev@users.noreply.github.com>
2024-02-04 16:05:31 +0100

    Update mongodb-connector-local.js

---
+++ backend/src/javascript/mongodb-connector-local.js
@@ -0,0 +1,18 @@
+const { MongoClient } = require("mongodb");
+
+async function connectToDatabase() {
+  const uri = "mongodb://localhost:27017";
+  const client = new MongoClient(uri);
+
+  try {
+    console.log("Tentative de connexion à la base de données");
+    await client.connect();
+    console.log("Connecté à la base de données");
+  } catch (error) {
+    console.error("Erreur de connexion à la base de données", error);
+  }finally {
+    await client.close();
+  }
+}
+
+connectToDatabase();
